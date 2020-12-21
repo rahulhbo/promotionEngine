a0579c9ea7c01a6bc62defe96eb2228c3ea86f48
rahul.yadav <rahul.yadav@DESKTOP-II46U96>
2020-12-22 00:29:38 +0530

    initial Commit

--- src/main/java/com/prmotioneng/model/Cart.java
+++ src/main/java/com/prmotioneng/model/Cart.java
@@ -1,5 +1,25 @@
 package com.prmotioneng.model;
 
+import java.util.*;
+
 public class Cart {
+	 private Map<Item, Integer> itemsWithQuantity;
+	 
+	 
+
+	public Cart(Map<Item, Integer> itemsWithQuantity) {
+		super();
+		this.itemsWithQuantity = itemsWithQuantity;
+	}
+
+	public Map<Item, Integer> getItemsWithQuantity() {
+		return itemsWithQuantity;
+	}
+
+	public void setItemsWithQuantity(Map<Item, Integer> itemsWithQuantity) {
+		this.itemsWithQuantity = itemsWithQuantity;
+	}
+	 
+	 
 
 }
